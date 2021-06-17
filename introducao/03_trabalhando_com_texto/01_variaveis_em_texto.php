f9cd457e48358ea5e17bcedebd911753f9cbc88e
Alessandro Almeida <43619109+asalmeidarj@users.noreply.github.com>
2021-06-17 20:58:43 -0300

    Create 01_variaveis_em_texto.php

---
+++ introducao/03_trabalhando_com_texto/01_variaveis_em_texto.php
@@ -0,0 +1,23 @@
+<?php
+
+// EXIBINDO O VALOR DE UMA VARIÁVEL NO TEXTO
+
+$nome = "Alessandro";
+$idade = 32;
+
+/*
+  Com as aspas simples o resultado exibido será
+  exatamente o mesmo dentro das aspas
+*/
+echo 'Me chamo $nome e tenho $idade anos';
+
+/* 
+  Se utilizássemos aspas duplas seria exibido o valor atribuído à variável
+*/
+echo "Me chamo $nome e tenho $idade anos"; 
+// resultado: Me chamo Alessandro e tenho 32 anos
+
+
+
+
+ 
