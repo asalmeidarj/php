8f7c8937391807dedcc19b0fbcc3c77b2d774144
Alessandro Almeida <43619109+asalmeidarj@users.noreply.github.com>
2021-06-17 20:36:27 -0300

    Create operacoes.php

---
+++ introducao/operacoes.php
@@ -0,0 +1,24 @@
+<?php
+
+// Operações matemáticas básicas com PHP
+
+//SOMA
+$num1 = 36;
+$num2 = 3;
+
+$soma = $num1 + $num2; // $soma recebe o valor 39 (36+3)
+
+//SUBTRAÇÃO
+$subtracao = $num1 - $num2; // $subtracao recebe o valor 33 (36-3)
+
+//MULTIPLICAÇÃO
+$multiplicacao = $num1 * $num2; // $multiplicação recebe o valor 108 (36 vezes 3)
+
+//DIVISÃO
+$divisao = $num1 / $num2; // $divisao == 12 (36 dividido por 3)
+
+//RESTO
+$resto = $num1 % $num2; // $resto recebe o resto da divisão de 36 por 3, ou seja, 0.
+
+//POTÊNCIA
+$potencia = 5 ** 3 // $potencia recebe o resultado de 3 elevado ao cubo (125).
