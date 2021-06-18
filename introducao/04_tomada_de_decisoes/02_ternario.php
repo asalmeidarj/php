ccec8a1d3a5d6c00d086aab04b7a92e7c677ff53
Alessandro Almeida <43619109+asalmeidarj@users.noreply.github.com>
2021-06-17 21:49:43 -0300

    Create 02_ternario.php

---
+++ introducao/04_tomada_de_decisoes/02_ternario.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = 'Alessandro';
+
+// ESTRUTURA 
+// (VARIÁVEL) = (CONDIÇÃO) ? (valorseverdadeiro) : (valorsefalso); 
+$meunome = $nome == 'Alessandro' ? "Meu nome é $nome" : "$nome não é o meu nome";
+
+/*
+  Como  $nome recebeu 'Alessandro' então a condição $nome == 'Alessandro'
+  é verdadeira, ou seja, $meunome recebeu "Meu nome é Alessandro"
+*/
