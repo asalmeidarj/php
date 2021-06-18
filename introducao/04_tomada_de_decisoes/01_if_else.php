aa0b6c71e3b575f442abddccecbe492260706271
Alessandro Almeida <43619109+asalmeidarj@users.noreply.github.com>
2021-06-17 21:35:05 -0300

    Create 01_if_else.php

---
+++ introducao/04_tomada_de_decisoes/01_if_else.php
@@ -0,0 +1,14 @@
+<?php
+
+// Executar um comando com uma condição.
+
+$idade = 32;
+
+// Condição: idade maior do que ou igual a 18.
+if ($idade>=18){ 
+  // Mensagem exibida ser a condição for verdadeira
+  echo "A idade é maior do que ou igual a 18 anos";
+} else {
+  // Mensagem exibida ser a condição for falsa
+  echo "A idade é menor do que 18 anos";
+};
